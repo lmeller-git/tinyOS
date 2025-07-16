96d3b2c9e03fa440163ce0f2346be9af38fc34c5
lmeller-git <152814119+lmeller-git@users.noreply.github.com>
2025-07-16 12:22:11 +0200

    Delete kernel/src/include_bins.rs

--- kernel/src/include_bins.rs
+++
@@ -1,6 +0,0 @@
-pub fn get_binaries() -> alloc::vec::Vec<&'static [u8]>{
-	alloc::vec![
-		include_bytes!("/home/louismeller/Coding/os/tinyOS/kernel/../tinyosprograms/programs/example-asm/a.out"),
-		include_bytes!("/home/louismeller/Coding/os/tinyOS/kernel/../tinyosprograms/programs/example-rs/a.out"),
-	]
-}
