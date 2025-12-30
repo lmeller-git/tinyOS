a40790aef44130094bb713d0a9aad7b815074b39
lmeller-git <louis.meller@icloud.com>
2025-12-30 21:12:27 +0100

    rename nblfq to nblfqueue

--- kernel/src/kernel/threading/wait/mod.rs
+++ kernel/src/kernel/threading/wait/mod.rs
@@ -9,7 +9,7 @@ use core::{
 use atomic_pool::{Pool, pool};
 use conquer_once::spin::OnceCell;
 use hashbrown::HashMap;
-use nblfq::{
+use nblfqueue::{
     MPMCQueue,
     array::StaticQueue,
     slot::{PtrLike, TaggedPtr64},
